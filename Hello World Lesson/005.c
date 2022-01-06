1cb5da13f932235a451c16d65833314f7bc502f0
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-06 18:44:11 -0300

    Compiling
    Usamos o terminal para essa parte.

---
+++ Hello World Lesson/005.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  // output a line
+  printf("Hello World!\n");
+}
+
+// The compiler is the program that converts your code to an executable program that can be run on your computer. This involves reading the code from a file and compiling it into code the computer processor can run.
