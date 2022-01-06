11ff0177388b2bb62136dc774c05447bb6bced19
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-06 18:39:08 -0300

    Comments

---
+++ Hello World Lesson/004.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  // I don't like Cookies 
+  printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+  /* I'm reviewing this content */
+  printf("Learning the C programming language is an exciting adventure!\n");
+}
+
+// Line Comment
+/* Block comment */
