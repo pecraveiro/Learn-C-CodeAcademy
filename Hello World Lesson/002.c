69df31b1942120aa708362138681f46c83b8d488
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-06 18:31:05 -0300

    Syntax and Errors

---
+++ Hello World Lesson/002.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() { // Função principal
+  // output a line
+  printf("Hello World!\n"); // Prinft is a statement
+}
+
+// The only lines of text that can change case are the comment and the text between quotes.
+// Statement - ''Declaração'' - Statement é uma declaração, pela tradução literal. É uma afirmação. Muitas vezes chamamos em português de comando, ainda que a palavra não expresse bem o que é. Normalmente usado em linguagens imperativas (quase todas mainstream) e é todo o necessário para completar uma instrução completa para o computador executar.
+// All strings must be surrounded by double-quotes.
