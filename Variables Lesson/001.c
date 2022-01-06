14fe851f34cb94d7296d9aadcc2dea4a486aaa5a
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2022-01-06 19:08:57 -0300

    Naming Standards
    Restrições dos nomes

---
+++ Variables Lesson/001.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+  // Criação e organização das variáveis conforme o pedido
+  int number1;
+  int test_value;
+  int example1;
+  
+  printf("Hello World!\n");
+
+}
